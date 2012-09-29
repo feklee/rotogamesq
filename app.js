96a46dff65e6454bb4107607cfd44a04b7e29c92
Felix E. Klee <felix.klee@inka.de>
2012-09-29 10:48:52 +0200

    Remove js13kGames specific code

--- app.js
+++ app.js
@@ -19,7 +19,6 @@
 'use strict';
 
 var express = require('express'),
-    routes = require('./routes'),
     http = require('http'),
     path = require('path'),
     app = express();
@@ -40,7 +39,9 @@ app.configure('development', function () {
     app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function (req, res) {
+    res.render('index');
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
