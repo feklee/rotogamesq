e3edfa85e6d475fb284d554f592a0f752eb9bb8a
Felix E. Klee <felix.klee@inka.de>
2012-12-24 17:54:29 +0100

    Remove warning: possible EventEmitter memory leak

--- app/server/hiscores_factory.js
+++ app/server/hiscores_factory.js
@@ -147,6 +147,8 @@ listen = function (socket, board) {
     redisClient.on('connect', function () {
         onRedisConnect.call(this, socket, board);
     });
+    socket.setMaxListeners('disconnect', 0); // to avoid warning if there are
+                                             // more than 10 boards
     socket.once('disconnect', function () {
         onDisconnect.call(this, socket, board);
     });
