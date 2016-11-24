599bfb0e942639e513fa8173d6b00b1d2ef5a524
Felix E. Klee <felix.klee@inka.de>
2016-11-24 22:39:42 +0100

    Finish reformatting common code for JSLint

--- app/common/tiles_factory.js
+++ app/common/tiles_factory.js
@@ -12,53 +12,94 @@ try {
 }
 
 commonDefine(function () {
-    'use strict';
+    "use strict";
 
-    var prototype, rotate, rotateInverse, rotateWithRotator, selectedTiles,
-        rotator90CW, rotator90CCW, rotator180, rgb, createFromCtx,
-        createFromImgData, areEqual;
-
-    selectedTiles = function (x1T, y1T, x2T, y2T) {
-        var sTiles, sTilesColumn, xT, yT;
-
-        sTiles = [];
-        for (xT = x1T; xT <= x2T; xT += 1) {
-            sTilesColumn = [];
-            for (yT = y1T; yT <= y2T; yT += 1) {
-                sTilesColumn.push(this[xT][yT]);
+    var columnsAreEqual = function (tiles1Column, tiles2Column, isEqual) {
+        var yT = 0;
+        while (yT < tiles2Column.length) {
+            if (!isEqual(tiles2Column[yT], tiles1Column[yT])) {
+                return false;
             }
-            sTiles.push(sTilesColumn);
+            yT += 1;
+        }
+        return true;
+    };
+
+    var areEqual = function (tiles1, tiles2, isEqual) {
+        if (tiles2.widthT !== tiles1.widthT ||
+                tiles2.heightT !== tiles1.heightT) {
+            return false;
+        }
+
+        var xT = 0;
+        while (xT < tiles2.length) {
+            if (!columnsAreEqual(tiles1[xT], tiles2[xT], isEqual)) {
+                return false;
+            }
+            xT += 1;
+        }
+
+        return true;
+    };
+
+    var selectedTilesInColumn = function (tiles, xT, y1T, y2T) {
+        var sTilesColumn = [];
+        var yT = y1T;
+        while (yT <= y2T) {
+            sTilesColumn.push(tiles[xT][yT]);
+            yT += 1;
+        }
+        return sTilesColumn;
+    };
+
+    var selectedTiles = function (tiles, x1T, y1T, x2T, y2T) {
+        var sTiles = [];
+        var xT = x1T;
+        while (xT <= x2T) {
+            sTiles.push(selectedTilesInColumn(tiles, xT, y1T, y2T));
+            xT += 1;
         }
 
         return sTiles;
     };
 
-    rotator90CW = function (sTiles, xT, yT, dimensions) {
+    var rotator90CW = function (sTiles, xT, yT, dimensions) {
         return sTiles[yT][dimensions.widthT - xT];
     };
 
-    rotator90CCW = function (sTiles, xT, yT, dimensions) {
+    var rotator90CCW = function (sTiles, xT, yT, dimensions) {
         return sTiles[dimensions.heightT - yT][xT];
     };
 
-    rotator180 = function (sTiles, xT, yT, dimensions) {
+    var rotator180 = function (sTiles, xT, yT, dimensions) {
         return sTiles[dimensions.widthT - xT][dimensions.heightT - yT];
     };
 
-    rotateWithRotator = function (rectT, rotator) {
-        var xT, yT,
-            x1T = rectT[0][0], y1T = rectT[0][1],
-            x2T = rectT[1][0], y2T = rectT[1][1],
-            dimensions = {
-                widthT: x2T - x1T,
-                heightT: y2T - y1T
-            },
-            sTiles = selectedTiles.call(this, x1T, y1T, x2T, y2T);
+    var rotateColumnWithRotator = function (xT, tiles, sTiles, x1T, y1T, y2T,
+            dimensions, rotator) {
+        var yT = y1T;
+        while (yT <= y2T) {
+            tiles[xT][yT] = rotator(sTiles, xT - x1T, yT - y1T, dimensions);
+            yT += 1;
+        }
+    };
 
-        for (xT = x1T; xT <= x2T; xT += 1) {
-            for (yT = y1T; yT <= y2T; yT += 1) {
-                this[xT][yT] = rotator(sTiles, xT - x1T, yT - y1T, dimensions);
-            }
+    var rotateWithRotator = function (tiles, rectT, rotator) {
+        var x1T = rectT[0][0];
+        var y1T = rectT[0][1];
+        var x2T = rectT[1][0];
+        var y2T = rectT[1][1];
+        var dimensions = {
+            widthT: x2T - x1T,
+            heightT: y2T - y1T
+        };
+        var sTiles = selectedTiles(tiles, x1T, y1T, x2T, y2T);
+
+        var xT = x1T;
+        while (xT <= x2T) {
+            rotateColumnWithRotator(xT, tiles, sTiles, x1T, y1T, y2T,
+                    dimensions, rotator);
+            xT += 1;
         }
     };
 
@@ -67,31 +108,94 @@ commonDefine(function () {
     //
     // The rectangle is defined by its top left and its bottom right corner, in
     // that order.
-    rotate = function (rotation) {
-        var rectT = rotation.rectT, cw = rotation.cw;
+    var rotate = function (tiles, rotation) {
+        var rectT = rotation.rectT;
+        var cw = rotation.cw;
 
         if (rectT.isSquare) {
             if (cw) {
-                rotateWithRotator.call(this, rectT, rotator90CW);
+                rotateWithRotator(tiles, rectT, rotator90CW);
             } else {
-                rotateWithRotator.call(this, rectT, rotator90CCW);
+                rotateWithRotator(tiles, rectT, rotator90CCW);
             }
         } else {
-            rotateWithRotator.call(this, rectT, rotator180);
+            rotateWithRotator(tiles, rectT, rotator180);
         }
     };
 
     // Applies the inverse of the specified rotation.
-    rotateInverse = function (rotation) {
-        rotate.call(this, rotation.inverse);
+    var rotateInverse = function (tiles, rotation) {
+        rotate(tiles, rotation.inverse);
     };
 
     // Returns the specified triple as RGB string.
-    rgb = function (imgData, offs) {
-        return ('rgb(' +
-                imgData[offs] + ',' +
-                imgData[offs + 1] + ',' +
-                imgData[offs + 2] + ')');
+    var rgb = function (imgData, offs) {
+        return ("rgb(" +
+                imgData[offs] + "," +
+                imgData[offs + 1] + "," +
+                imgData[offs + 2] + ")");
+    };
+
+    var createColumnFromCtx = function (xT, sideLenT, imgData) {
+        var tilesColumn = [];
+        var offs;
+        var yT = 0;
+        while (yT < sideLenT) {
+            offs = 4 * (yT * sideLenT + xT);
+            tilesColumn.push({
+                color: rgb(imgData, offs)
+            });
+            yT += 1;
+        }
+        return tilesColumn;
+    };
+
+    var create;
+    create = function () {
+        var newTiles = Object.create([]);
+
+        return Object.defineProperties(newTiles, {
+            areEqualTo: {value: function (tiles) {
+                return areEqual(newTiles, tiles, function (tile1, tile2) {
+                    return tile1 === tile2;
+                });
+            }},
+
+            colorsAreEqualTo: {value: function (tiles) {
+                return areEqual(newTiles, tiles, function (tile1, tile2) {
+                    return tile1.color === tile2.color;
+                });
+            }},
+
+            widthT: {get: function () {
+                return newTiles.length;
+            }},
+
+            heightT: {get: function () {
+                return newTiles.widthT > 0
+                    ? newTiles[0].length
+                    : 0;
+            }},
+
+            // Returns a deep copy of the object.
+            copy: {value: function () {
+                var copiedTiles = create();
+
+                newTiles.forEach(function (thisColumn) {
+                    copiedTiles.push(thisColumn.slice());
+                });
+
+                return copiedTiles;
+            }},
+
+            rotate: {value: function (rotation) {
+                rotate(newTiles, rotation);
+            }},
+
+            rotateInverse: {value: function (rotation) {
+                rotateInverse(newTiles, rotation);
+            }}
+        });
     };
 
     // Creates tiles (each identified by a color specifier), describing the
@@ -105,104 +209,51 @@ commonDefine(function () {
     // Tile colors are stored in objects and not directly as value of a tile.
     // This makes it possible to differentiate between tiles that have the same
     // color (when comparing them using e.g. `===`).
-    createFromCtx = function (ctx, posT, sideLenT) {
-        var tiles, tilesColumn, xT, yT, offs,
-            imgData = ctx.getImageData(posT[0], posT[1],
-                                       sideLenT, sideLenT).data;
-
-        tiles = Object.create(prototype);
-        for (xT = 0; xT < sideLenT; xT += 1) {
-            tilesColumn = [];
-            for (yT = 0; yT < sideLenT; yT += 1) {
-                offs = 4 * (yT * sideLenT + xT);
-                tilesColumn.push({
-                    color: rgb(imgData, offs)
-                });
-            }
-            tiles.push(tilesColumn);
+    var createFromCtx = function (ctx, posT, sideLenT) {
+        var imgData = ctx.getImageData(
+            posT[0],
+            posT[1],
+            sideLenT,
+            sideLenT
+        ).data;
+        var tiles = create();
+        var xT = 0;
+        while (xT < sideLenT) {
+            tiles.push(createColumnFromCtx(xT, sideLenT, imgData));
+            xT += 1;
         }
 
         return tiles;
+    };
+
+    var createColumnFromImgData = function (xT, posT, sideLenT, imgWidth,
+            imgData) {
+        var tilesColumn = [];
+        var offs;
+        var yT = posT[1];
+        while (yT < posT[1] + sideLenT) {
+            offs = 4 * (yT * imgWidth + xT);
+            tilesColumn.push({
+                color: rgb(imgData, offs)
+            });
+            yT += 1;
+        }
+        return tilesColumn;
     };
 
     // Creates tiles from image data, as created with png.js for Node.js:
     //
     // <https://github.com/devongovett/png.js/>
-    createFromImgData = function (imgData, imgWidth, posT, sideLenT) {
-        var tiles, tilesColumn, xT, yT, offs;
-
-        tiles = Object.create(prototype);
-        for (xT = posT[0]; xT < posT[0] + sideLenT; xT += 1) {
-            tilesColumn = [];
-            for (yT = posT[1]; yT < posT[1] + sideLenT; yT += 1) {
-                offs = 4 * (yT * imgWidth + xT);
-                tilesColumn.push({
-                    color: rgb(imgData, offs)
-                });
-            }
-            tiles.push(tilesColumn);
+    var createFromImgData = function (imgData, imgWidth, posT, sideLenT) {
+        var tiles = create();
+        var xT = posT[0];
+        while (xT < posT[0] + sideLenT) {
+            tiles.push(createColumnFromImgData(xT, posT, sideLenT, imgWidth,
+                    imgData));
+            xT += 1;
         }
-
         return tiles;
     };
-
-    areEqual = function (tiles1, tiles2, isEqual) {
-        var xT, yT, tiles2Column, tiles1Column;
-
-        if (tiles2.widthT !== tiles1.widthT ||
-                tiles2.heightT !== tiles1.heightT) {
-            return false;
-        }
-
-        for (xT = 0; xT < tiles2.length; xT += 1) {
-            tiles2Column = tiles2[xT];
-            tiles1Column = tiles1[xT];
-            for (yT = 0; yT < tiles2Column.length; yT += 1) {
-                if (!isEqual(tiles2Column[yT], tiles1Column[yT])) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    };
-
-    prototype = Object.create([], {
-        areEqualTo: {value: function (tiles) {
-            return areEqual(this, tiles, function (tile1, tile2) {
-                return tile1 === tile2;
-            });
-        }},
-
-        colorsAreEqualTo: {value: function (tiles) {
-            return areEqual(this, tiles, function (tile1, tile2) {
-                return tile1.color === tile2.color;
-            });
-        }},
-
-        widthT: {get: function () {
-            return this.length;
-        }},
-
-        heightT: {get: function () {
-            return this.widthT > 0 ? this[0].length : 0;
-        }},
-
-        // Returns a deep copy of `this`.
-        copy: {value: function () {
-            var newTiles = Object.create(prototype);
-
-            this.forEach(function (thisColumn) {
-                newTiles.push(thisColumn.slice());
-            });
-
-            return newTiles;
-        }},
-
-        rotate: {value: rotate},
-
-        rotateInverse: {value: rotateInverse}
-    });
 
     return Object.create(null, {
         createFromCtx: {value: createFromCtx},
